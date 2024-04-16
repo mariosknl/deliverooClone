9f6854f86618da40fdcbccadfc24a4ef6dffb612
mariosknl <mariosknl@outlook.com>
2024-04-16 17:28:43 +0300

    section list for details item

--- app/details.tsx
+++ app/details.tsx
@@ -1,12 +1,26 @@
-import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
+import {
+	View,
+	Text,
+	StyleSheet,
+	Image,
+	TouchableOpacity,
+	SectionList,
+	ListRenderItem,
+} from "react-native";
 import ParallaxScrollView from "@/components/ParallaxScrollview";
 import Colors from "@/constants/Colors";
-import { restaurants } from "@/assets/data/home";
+import { restaurant } from "@/assets/data/restaurant";
 import { useLayoutEffect } from "react";
-import { useNavigation } from "expo-router";
+import { Link, useNavigation } from "expo-router";
 import { Ionicons } from "@expo/vector-icons";
 const Details = () => {
 	const navigation = useNavigation();
+
+	const DATA = restaurant.food.map((item, index) => ({
+		title: item.category,
+		data: item.meals,
+		index,
+	}));
 
 	useLayoutEffect(() => {
 		navigation.setOptions({
@@ -35,6 +49,19 @@ const Details = () => {
 		});
 	}, []);
 
+	const renderItem: ListRenderItem<any> = ({ item, index }) => (
+		<Link href={"/"} asChild>
+			<TouchableOpacity key={index} style={styles.item}>
+				<View style={{ flex: 1 }}>
+					<Text style={styles.dish}>{item.name}</Text>
+					<Text style={styles.dishText}>{item.info}</Text>
+					<Text style={styles.dishText}>€{item.price}</Text>
+				</View>
+				<Image source={item.img} style={styles.dishImage} />
+			</TouchableOpacity>
+		</Link>
+	);
+
 	return (
 		<>
 			<ParallaxScrollView
@@ -45,18 +72,49 @@ const Details = () => {
 				contentBackgroundColor={Colors.lightGrey}
 				renderBackground={() => (
 					<Image
-						source={restaurants[0].img}
+						source={restaurant.img}
 						style={{ width: "100%", height: 300 }}
 					/>
 				)}
 				renderStickyHeader={() => (
 					<View key="sticky-header" style={styles.stickySection}>
-						<Text style={styles.stickySectionText}>{restaurants[0].name}</Text>
+						<Text style={styles.stickySectionText}>{restaurant.name}</Text>
 					</View>
 				)}
 			>
 				<View style={styles.detailsContainer}>
-					<Text>Details</Text>
+					<Text style={styles.restaurantName}>{restaurant.name}</Text>
+					<Text style={styles.restaurantDescription}>{restaurant.about}</Text>
+					<Text style={styles.restaurantDescription}>
+						{restaurant.delivery} ·{" "}
+						{restaurant.tags.map(
+							(tag, index) =>
+								`${tag}${index < restaurant.tags.length - 1 ? " · " : ""}`
+						)}
+					</Text>
+					<Text>{restaurant.about}</Text>
+					<SectionList
+						contentContainerStyle={{ paddingBottom: 50 }}
+						keyExtractor={(item, index) => `${item.id}-${index}`}
+						scrollEnabled={false}
+						sections={DATA}
+						renderItem={renderItem}
+						renderSectionHeader={({ section: { title, index } }) => (
+							<Text style={styles.sectionHeader}>{title}</Text>
+						)}
+						ItemSeparatorComponent={() => (
+							<View
+								style={{
+									height: 1,
+									backgroundColor: Colors.grey,
+									marginHorizontal: 16,
+								}}
+							/>
+						)}
+						SectionSeparatorComponent={() => (
+							<View style={{ height: 1, backgroundColor: Colors.grey }} />
+						)}
+					/>
 				</View>
 			</ParallaxScrollView>
 		</>
@@ -90,6 +148,44 @@ const styles = StyleSheet.create({
 		flexDirection: "row",
 		alignItems: "center",
 		justifyContent: "space-between",
+	},
+	restaurantName: {
+		fontSize: 30,
+		margin: 16,
+	},
+	restaurantDescription: {
+		fontSize: 16,
+		margin: 16,
+		lineHeight: 22,
+		color: Colors.medium,
+	},
+	sectionHeader: {
+		fontSize: 22,
+		fontWeight: "bold",
+		margin: 16,
+		marginTop: 40,
+	},
+	item: {
+		backgroundColor: "white",
+		padding: 16,
+		flexDirection: "row",
+		justifyContent: "space-between",
+		marginHorizontal: 16,
+		marginVertical: 8,
+	},
+	dishImage: {
+		width: 80,
+		height: 80,
+		borderRadius: 4,
+	},
+	dish: {
+		fontSize: 16,
+		fontWeight: "bold",
+	},
+	dishText: {
+		fontSize: 14,
+		color: Colors.mediumDark,
+		paddingVertical: 4,
 	},
 });
 export default Details;
