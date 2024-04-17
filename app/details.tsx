08abf507449f49491817c64adacd43e35fc05984
mariosknl <mariosknl@outlook.com>
2024-04-17 09:45:28 +0300

    modal with animation for details page & expo haptics installed

--- app/details.tsx
+++ app/details.tsx
@@ -87,7 +87,7 @@ const Details = () => {
 	};
 
 	const renderItem: ListRenderItem<any> = ({ item, index }) => (
-		<Link href={"/"} asChild>
+		<Link href={{ pathname: "/(modal)/dish", params: { id: item.id } }} asChild>
 			<TouchableOpacity key={index} style={styles.item}>
 				<View style={{ flex: 1 }}>
 					<Text style={styles.dish}>{item.name}</Text>
