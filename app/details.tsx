33bb78503115ae0299f4a47ce90ecdc8c2183129
mariosknl <mariosknl@outlook.com>
2024-04-17 09:08:09 +0300

    sticky segments implemented

--- app/details.tsx
+++ app/details.tsx
@@ -6,15 +6,30 @@ import {
 	TouchableOpacity,
 	SectionList,
 	ListRenderItem,
+	ScrollView,
 } from "react-native";
 import ParallaxScrollView from "@/components/ParallaxScrollview";
 import Colors from "@/constants/Colors";
 import { restaurant } from "@/assets/data/restaurant";
-import { useLayoutEffect } from "react";
+import { useLayoutEffect, useRef, useState } from "react";
 import { Link, useNavigation } from "expo-router";
 import { Ionicons } from "@expo/vector-icons";
+import Animated, {
+	useAnimatedStyle,
+	useSharedValue,
+	withTiming,
+} from "react-native-reanimated";
 const Details = () => {
 	const navigation = useNavigation();
+	const [activeIndex, setActiveIndex] = useState(0);
+
+	const opacity = useSharedValue(0);
+	const animatedStyles = useAnimatedStyle(() => ({
+		opacity: opacity.value,
+	}));
+
+	const scrollRef = useRef<ScrollView>(null);
+	const itemsRef = useRef<TouchableOpacity[]>([]);
 
 	const DATA = restaurant.food.map((item, index) => ({
 		title: item.category,
@@ -49,6 +64,28 @@ const Details = () => {
 		});
 	}, []);
 
+	const selectCategory = (index: number) => {
+		const selected = itemsRef.current[index];
+		setActiveIndex(index);
+
+		selected.measure((x) => {
+			scrollRef.current?.scrollTo({
+				x: x - 16,
+				y: 0,
+				animated: true,
+			});
+		});
+	};
+
+	const onScroll = (event: any) => {
+		const y = event.nativeEvent.contentOffset.y;
+		if (y > 350) {
+			opacity.value = withTiming(1);
+		} else {
+			opacity.value = withTiming(0);
+		}
+	};
+
 	const renderItem: ListRenderItem<any> = ({ item, index }) => (
 		<Link href={"/"} asChild>
 			<TouchableOpacity key={index} style={styles.item}>
@@ -66,6 +103,7 @@ const Details = () => {
 		<>
 			<ParallaxScrollView
 				backgroundColor={"#fff"}
+				scrollEvent={onScroll}
 				style={{ flex: 1 }}
 				parallaxHeaderHeight={250}
 				stickyHeaderHeight={100}
@@ -117,6 +155,40 @@ const Details = () => {
 					/>
 				</View>
 			</ParallaxScrollView>
+
+			<Animated.View style={[styles.stickySegments, animatedStyles]}>
+				<View style={styles.segmentsShadow}>
+					<ScrollView
+						ref={scrollRef}
+						horizontal
+						showsHorizontalScrollIndicator={false}
+						contentContainerStyle={styles.segmentScrollview}
+					>
+						{restaurant.food.map((item, index) => (
+							<TouchableOpacity
+								key={index}
+								ref={(ref) => (itemsRef.current[index] = ref!)}
+								style={
+									activeIndex === index
+										? styles.segmentButtonActive
+										: styles.segmentButton
+								}
+								onPress={() => selectCategory(index)}
+							>
+								<Text
+									style={
+										activeIndex === index
+											? styles.segmentTextActive
+											: styles.segmentText
+									}
+								>
+									{item.category}
+								</Text>
+							</TouchableOpacity>
+						))}
+					</ScrollView>
+				</View>
+			</Animated.View>
 		</>
 	);
 };
@@ -186,6 +258,57 @@ const styles = StyleSheet.create({
 		fontSize: 14,
 		color: Colors.mediumDark,
 		paddingVertical: 4,
+	},
+	stickySegments: {
+		position: "absolute",
+		height: 50,
+		left: 0,
+		right: 0,
+		top: 100,
+		backgroundColor: "#fff",
+		overflow: "hidden",
+		paddingBottom: 10,
+	},
+	segmentsShadow: {
+		backgroundColor: "#fff",
+		justifyContent: "center",
+		shadowColor: "#000",
+		shadowOffset: {
+			width: 0,
+			height: 5,
+		},
+		shadowOpacity: 0.1,
+		shadowRadius: 4,
+		elevation: 5,
+		width: "100%",
+		height: "100%",
+	},
+	segmentButton: {
+		paddingHorizontal: 16,
+		paddingVertical: 4,
+		borderRadius: 20,
+	},
+	segmentText: {
+		color: Colors.primary,
+		fontSize: 16,
+	},
+	segmentButtonActive: {
+		backgroundColor: Colors.primary,
+		paddingHorizontal: 16,
+		paddingVertical: 4,
+		borderRadius: 20,
+		marginHorizontal: 8,
+	},
+	segmentTextActive: {
+		color: "#fff",
+		fontWeight: "bold",
+		fontSize: 16,
+	},
+	segmentScrollview: {
+		paddingHorizontal: 16,
+		alignItems: "center",
+		gap: 20,
+		paddingBottom: 4,
 	},
 });
 export default Details;
